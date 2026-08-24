9aca0771b29c0d75d6b5420a0358cf68f2a79ebb
unknown <rk7312899@gmail.com>
2026-08-24 13:11:03 +0530

    new update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,7 @@ export const metadata: Metadata = {
   title: 'Viros GST Billing | Complete GST ERP for Indian Businesses',
   description: 'Complete GST ERP Software for Indian Businesses - Invoicing, Inventory, Purchases & GST Reporting',
   icons: {
-    icon: [
-      { url: '/logo.png', type: 'image/png' },
-      { url: '/favicon.ico', type: 'image/x-icon' },
-    ],
+    icon: '/logo.png',
     shortcut: '/logo.png',
     apple: '/logo.png',
   },
